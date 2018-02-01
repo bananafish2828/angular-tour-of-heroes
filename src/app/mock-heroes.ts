30741c56708085a349f1f6a97a1ff45f759c8166
bananafish2828 <bananafish2828@gmail.com>
2018-02-01 01:17:21 -0800

    angular2 tutorial

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Wolverine' },
+  { id: 12, name: 'Natasha Romanova' },
+  { id: 13, name: 'Wanda Maximova' },
+  { id: 14, name: 'Elektra' },
+  { id: 15, name: 'Sue Storm' },
+  { id: 16, name: 'Hulk' },
+  { id: 17, name: 'Thor Odinson' },
+  { id: 18, name: 'Motoko Kusanagi' },
+  { id: 19, name: 'Naruto Uzumaki' },
+  { id: 20, name: 'Hinata Hyuga' }
+];
